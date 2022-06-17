007d64e32fb336197e04bfc801256890a34d4246
CShingiro <nemyent@gmail.com>
2022-06-16 23:53:38 -0400

    Patch to mobile site home page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,7 +3,12 @@ import HomePageItems from 'src/components/HomePageItems.vue';
 </script>
 
 <template>
-  <q-page class="row inline items-center justify-evenly q-pa-md q-gutter-md">
+  <q-page
+    class="row inline items-center justify-evenly q-pa-md q-gutter-md gt-xs"
+  >
+    <home-page-items></home-page-items>
+  </q-page>
+  <q-page class="row inline items-center justify-evenly lt-sm">
     <home-page-items></home-page-items>
   </q-page>
 </template>
